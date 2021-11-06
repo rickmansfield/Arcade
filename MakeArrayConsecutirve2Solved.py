499d7306a600198247ef6d84cff02d0452615e7b
Rick <ricksmycodeguy@gmail.com>
2021-11-06 11:43:57 -0500

    one liner solution

--- MakeArrayConsecutirve2Solved.py
+++ MakeArrayConsecutirve2Solved.py
@@ -1,3 +1,13 @@
+"""
+Alternative. Not advise unless you are math oriented. 
+
+def makeArrayConsecutive2(statues):
+return (max(statues)-min(statues)+1)-len(statues)
+
+print(makeArrayConsecutive2([6, 2, 3, 8])) # 3
+print(makeArrayConsecutive2([0, 3])) # 2
+print(makeArrayConsecutive2([5, 4, 6])) # 0 
+"""
 def makeArrayConsecutive2(statues):
     # statues.sort()
     sorted_statues = sorted(statues)
