06c201f1d66ff355855369501b3319598a0f45e8
Rick <ricksmycodeguy@gmail.com>
2021-12-07 19:54:37 -0600

    adds explaination to shapeAreaPoly...

--- 5shapeAreaPolygoneInteresting.py
+++ 5shapeAreaPolygoneInteresting.py
@@ -1,3 +1,15 @@
+def sa(n):
+    count = 1
+    for n in range(1, n+1, 1):
+        print('numbers',n)
+        count += ((n-1)*4)
+    return count
+
+print('answer:', sa(2)); # 5
+print('answer:', sa(3)) # 13
+# print(sa(8000)) # 127984001
+
+# Explaination--------------------
 """
 since 1 ≤ n < 10^4.
 area = ((1 - 1) x 4) + 1
