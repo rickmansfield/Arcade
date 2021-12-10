1b4f64736d6c63a33683e636121db1038a5ca7aa
Rick <ricksmycodeguy@gmail.com>
2021-12-10 09:41:38 -0600

    practice

--- 5shapeAreaPolygoneInteresting.py
+++ 5shapeAreaPolygoneInteresting.py
@@ -1,10 +1,10 @@
 def sa(n):
     count = 1
     for n in range(1, n+1, 1):
-        print('numbers',n)
+        # print('numbers',n)
         count += ((n-1)*4)
     return count
-
+print('answer:', sa(1)); # 1
 print('answer:', sa(2)); # 5
 print('answer:', sa(3)) # 13
 # print(sa(8000)) # 127984001
@@ -30,5 +30,5 @@ def shapeArea(n):
         count += ((eachNum-1)*4)
         # print("Count inside loop", count)
     return count
-    
+  
 print(shapeArea(8000))
